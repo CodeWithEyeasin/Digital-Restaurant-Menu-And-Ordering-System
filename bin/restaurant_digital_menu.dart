5ccdf820496e6f9a50937f437d270656a7a525a6
MD Eyeasin Arafat <mdeyeasinarafat@MDs-MacBook-Air.local>
2023-11-23 23:32:43 +0600

    Initial commit

---
+++ bin/restaurant_digital_menu.dart
@@ -0,0 +1,5 @@
+import 'package:restaurant_digital_menu/restaurant_digital_menu.dart' as restaurant_digital_menu;
+
+void main(List<String> arguments) {
+  print('Hello world: ${restaurant_digital_menu.calculate()}!');
+}
---
+++ test/restaurant_digital_menu_test.dart
@@ -0,0 +1,8 @@
+import 'package:restaurant_digital_menu/restaurant_digital_menu.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
