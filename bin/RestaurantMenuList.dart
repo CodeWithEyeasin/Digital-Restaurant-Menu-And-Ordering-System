02af7bef549a78e78ec281072b985479868d1253
CodeWithEyeasin <mreyeasinarafat@gmail.com>
2023-11-24 16:27:48 +0600

    Project Updated

---
+++ bin/RestaurantMenuList.dart
@@ -0,0 +1,15 @@
+class RestaurantMenuList
+{
+  late double EspressoPrice=110,AmericanoPrice=150,CappuccinoPrice=190,HotChocolatePrice=190;
+  late int Espresso=0,Americano=0,Cappuccino=0,Hot_Chocolate=0;
+  void restaurant_menu()
+  {
+
+      print("1.Espresso------------------> $EspressoPrice");
+      print("2.Americano-----------------> $AmericanoPrice");
+      print("3.Cappuccino----------------> $CappuccinoPrice");
+      print("4.Hot Chocolate-------------> $HotChocolatePrice");
+      print("5.Order Done");
+
+  }
+}
--- bin/restaurant_digital_menu.dart
+++ bin/restaurant_digital_menu.dart
@@ -1,5 +1,11 @@
 import 'package:restaurant_digital_menu/restaurant_digital_menu.dart' as restaurant_digital_menu;
 
-void main(List<String> arguments) {
-  print('Hello world: ${restaurant_digital_menu.calculate()}!');
+import 'RestaurantMenuList.dart';
+
+void main(List<String> arguments)
+{
+  print("   ABC Restaurant Coffee Menu\nPlease Select Your Order Sir/Mam\n");
+  RestaurantMenuList menu=RestaurantMenuList();
+  menu.restaurant_menu();
+
 }
